f97ff34471686fe473871b22f6e4f40593702ade
hayrapetyansami <samihayrapetyan@gmail.com>
2022-06-15 20:26:22 +0400

    Project day 1

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 "use strict";
 
 const app = {
-    restaurantDetails: [
-        {address: "Երևան, Թումանյան 14"},
-        {telNumber:"015 12-23-45"},
-        {mail: "info@template.am"},
-        {fasebook: "https://facebook.com/"},
-        {instagram: "https://instagram.com/"},
-        {type: "Ռեստորան"},
-        {isDelivery : true},
-        {isOpen: true}
-    ],
-    restaurantStaff: [
-        {security: "Էդգար Գեվորգյան"},
+	restaurantDetails: [
+		{address: "Երևան, Թումանյան 14"},
+		{telNumber: "015 12-23-45"},
+		{mail: "info@template.am"},
+		{facebook: "http://facebook.com/"},
+		{instagram: "http://instagram.com/"},
+		{type: "Ռեստորան"},
+		{isDelivery: true},
+		{isOpen: true}
+	],
+	restaurantStaff: [
+		{security: "Էդգար Գեվորգյան"},
         {security: "Արսեն Ավագյան"},
         {waiter: "Մելքոն Դանիելյան"},
         {waiter: "Ալիսա Պողոսյան"},
@@ -25,20 +25,20 @@ const app = {
         {administrator: "Լիկա Ավագյանց"},
         {courier: "Սամսոն Խաչատրյան"},
         {hostess: "Ալիսա Սուրենյանց"},
-        {customer: "Մանե Սարգսյան"},
-    ],
-    restaurantMenu: [
-        {
-            type: "Տաք ուտեստներ",
+        {customer: "Մանե Սարգսյան"}
+	],
+	restaurantMenu: [
+		{
+			type: "Տաք ուտեստներ",
             item1: "Խոզի Խորոված",
-            item2: "Հավի Խորովաս",
+            item2: "Հավի Խորոված",
             item3: "Ճտի Տապակա ֆրիով",
             item4: "Ձկան Խորոված",
             item5: "Թփով Տոլմա",
             item6: "Կաղամբով Տոլմա",
-            item7: "Քյուֆթա",
-        },
-        {
+            item7: "Քյուֆթա"
+		},
+		{
             type: "Աղցաններ",
             item1: "Մայրաքաղաքային",
             item2: "Հունական",
@@ -46,9 +46,9 @@ const app = {
             item4: "Ցեզար",
             item5: "Վինեգրեդ",
             item6: "Խեցգետնով",
-            item7: "Բազուկով",
+            item7: "Բազուկով"
         },
-        {
+		{
             type: "Ալկոհոլային Խմիչքներ",
             item1: "Օղի",
             item2: "Գինի",
@@ -56,9 +56,9 @@ const app = {
             item4: "Վիսկի",
             item5: "Լիկյոր",
             item6: "Տեկիլա",
-            item7: "Ռոմ",
+            item7: "Ռոմ"
         },
-        {
+		{
             type: "Հյութեր և Զովացուցիչ Ըմպելիքներ",
             item1: "Տնական Կոմպոտ",
             item2: "Կոկա-Կոլա",
@@ -66,9 +66,9 @@ const app = {
             item4: "Սփրայթ",
             item5: "Սեվըն Ափ",
             item6: "Թան",
-            item7: "Լիմոնադ",
+            item7: "Լիմոնադ"
         },
-        {
+		{
             type: "Խմորեղեններ",
             item1: "Միկադո",
             item2: "Սնիկերս",
@@ -76,29 +76,42 @@ const app = {
             item4: "Տիրամիսու",
             item5: "Շոկոլադե",
             item6: "Մրգային",
-            item7: "Բրաունի",
+            item7: "Բրաունի"
         }
-    ],
-
-    isOpened(end) {
-        if (end >= 10) {
-            app.restaurantDetails[7].isOpen = false;
-        }
-    },
-
-    callCenter(address, telNumber, isDelivery, isOpen, type) {
-        return `
-            Բարև Ձեզ, Ձեզ սպասարկում է ${app.restaurantStaff[13].customer}ը
-            Մեր հասցեն է ${address = app.restaurantDetails[0].address}
-            ${telNumber = app.restaurantDetails[1].telNumber !== undefined ? "Մեր Հեռախոսահամարն է " + app.restaurantDetails[1].telNumber : "Կներեք, մենք չունենք հեռախոսահամար"}
-            ${isDelivery = app.restaurantDetails[6].isDelivery === true ? "Այո, կարող ենք իրականացնել Ձեր առաքումը" : "Կներեք, բայց մեր առաքիչը զբաղված է, պետք է դուք մոտենաք"}
-            ${isOpen = app.restaurantDetails[7].isOpen === true ? "Այո, ներկա պահին մենք բաց ենք" : "Ոչ, կներեք մենք փակ ենք"}
-            Մենք ներկայացնում ենք ${type = app.restaurantDetails[5].type}
-        `;
-    }
+	],
+	callCenter(address, telNumber, isDelivery, isOpen, type){
+		return `
+			Բարև Ձեզ, Ձեզ սպասարկում է ${app.restaurantStaff[13].customer}ը
+			${
+				address = app.restaurantDetails[0].address !== undefined ? 
+				"Մեր հասցեն է" : 
+				"Կներեք, մենք հասցե չունենք"
+			}
+			${
+				telNumber = app.restaurantDetails[1].telNumber !== undefined ? 
+				"Մեր հեռախոսահամարն է " + app.restaurantDetails[1].telNumber : 
+				"Կներեք, մենք չունենք հեռախոսահամար"
+			}
+			${
+				isDelivery = app.restaurantDetails[6].isDelivery === true ? 
+				"Այո, կարող ենք իրականացնել Ձեր պատվերը" :
+				"Կներեք, բայց մեր առաքիչը զբաղված է, պետք է դուք մոտենաք"
+			}
+			${
+				isOpen = app.restaurantDetails[7].isOpen === true ? 
+				"Այո, ներկա պահին բաց ենք" :
+				"Ոչ, կներեք մենք փակ ենք"
+			}`;
+	},
+	isOpened(start, end) {
+		start >= end ? 
+		app.restaurantDetails[7].isOpen = false : 
+		app.restaurantDetails[7].isOpen = true;
+	}
 };
 
-app.isOpened(10);
+app.isOpened(9, 24);
+
 const client = app.callCenter();
 
 console.log(client);
