b8ae0e73f1d7ec5227bdf0749c7992c80f92c617
hayrapetyansami <samihayrapetyan@gmail.com>
2022-08-31 13:57:48 +0400

    we updated our date file

--- date/date-base/js/script.js
+++ date/date-base/js/script.js
@@ -1,41 +1,58 @@
 "use strict";
 
-// մենք ստանում ենք դեյթան
 const date = new Date();
-console.log(date.getTime());
 
-const hours = date.getHours(); // ժամ
-const minutes = date.getMinutes(); // րոպե
-const seconds = date.getSeconds(); // վայրկյան
-const miliSeconds = date.getMilliseconds(); // միլիվայրկյան
+// DATE API
+console.log(date.getFullYear()); // թվականն ենք ստանում - 2022
+console.log(date.getMonth()); // ամիս -1
+console.log(date.getDay()); // ստանում ենք շաբաթվա օրըն կիրակին = 0
+console.log(date.getDate()); // ստանում ենք էս օրը, 23։59։59։999ից առաջ
 
-console.log(`${hours} : ${minutes} : ${seconds} : ${miliSeconds}`);
+// STANDART DATE & TIME API
+const dnt = new Date()
+console.log(dnt.toDateString()); // Wed Aug 31 2022
+console.log(dnt.toTimeString()); // 13:31:01 GMT+0400 (Armenia Standard Time)
 
-console.log(date.toLocaleString()); // Տարի, Ամիս, Օր, Ժամ, Րոպե, Վայրկյան
+// LOCAL DATE & TIME
+const localDNT = new Date();
+console.log(localDNT.toLocaleTimeString());
+console.log(localDNT.toLocaleDateString());
+console.log(localDNT.toLocaleString());
 
-const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-console.log(timezone); // Asia/Yerevan
+// TIME API
+const timeDate = new Date();
 
-console.log(date.getFullYear()); // թվական
-console.log(date.getMonth()); // ամիս + 1
-console.log(date.getDay()); // շաբաթվա օր, օրինակ կիրակին = 0 ?
-console.log(date.getDate()); // էսօրվա օրը / 23։59։59ից առաջ
+console.log(timeDate.getHours());          // ժամ
+console.log(timeDate.getMinutes());        // րոպե
+console.log(timeDate.getSeconds());        // վայրկյան
+console.log(timeDate.getMilliseconds());   // միլիվայրկյան
+console.log(timeDate.getTime());           // միլիվարկյան տվյալ պահից
+console.log(timeDate.getTimezoneOffset()); // ժամային գոտի -240 րոպե
 
-// մենք տալիս ենք մեր ուզած դեյթան
-const fakeDate = new Date();
-fakeDate.setFullYear(1993)
-fakeDate.setMonth(11)
-fakeDate.setDate(1)
-fakeDate.setHours(16)
-console.log(fakeDate);
+// TIME ZONE - Asia/Yerevan
+console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
+
+// SET
+const dSet = new Date();
+dSet.setFullYear(353);
+dSet.setMonth(7);
+dSet.setDate(15);
+dSet.setHours(21);
+dSet.setMinutes(24);
+dSet.setSeconds(53);
+dSet.setMilliseconds(999);
+
+console.log(dSet);
 
 // ստուգում ենք մեր կոդի <Performance>-ը
 let start = new Date();
 
+let some = 0;
 for (let i = 0; i < 500000; i++) {
-	let some = i ** 2;
+	some = i ** 2;
 }
+console.log(some);
 
-let finished = new Date();
+let end = new Date();
 
-console.log(`ժամանակը։ ${finished - start} միլիվայրկյան`);
+console.log(`ժամանակը։ ${end - start} միլիվայրկյան`);
