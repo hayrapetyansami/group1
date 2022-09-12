3af6a940e489d4e05d3c2fec8288cd0139797133
hayrapetyansami <samihayrapetyan@gmail.com>
2022-09-12 16:16:55 +0400

    edited function constructor

--- function-construction-old/js/script.js
+++ function-construction-old/js/script.js
@@ -2,13 +2,20 @@
 
 
 // ES5 Function Constructor
-function Man (fullname, age) {
+function Man (fullname, age, country) {
 	this.fullname = fullname;
 	this.age = age;
+	this.country = country;
+	this.sayCountry = function () {
+		console.log(country);
+	}
 }
 
-const alex = new Man("Alex John", 35);
+const alex = new Man("Alex John", 35, "USA");
 console.log(alex);
 
-const john = new Man("John Smith", 41);
+const john = new Man("John Smith", 41, "Canada");
 console.log(john);
+
+alex.sayCountry();
+john.sayCountry();
