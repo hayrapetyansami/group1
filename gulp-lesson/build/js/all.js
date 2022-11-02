1f7543ddade4b81d2de1ca0fe43081f2e860e946
hayrapetyansami <samihayrapetyan@gmail.com>
2022-11-02 17:04:44 +0400

    GULP lesson, gulpfile and gulp plugins + babel

---
+++ gulp-lesson/build/js/all.js
@@ -0,0 +1,2 @@
+"use strict";window.addEventListener("load",function(){console.log(15),console.log({name:"Alex",age:25})});
+//# sourceMappingURL=all.js.map
---
+++ gulp-lesson/src/js/index.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+	const a = 15;
+
+	const obj = {
+		name: "Alex", 
+		age: 25
+	};
+
+	const arr = [1, 5, 4, 8, 9, 7, 2, 3, 4, 5];
+
+	console.log(a);
+	console.log(obj);
+})
